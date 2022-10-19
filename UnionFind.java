89fecf6077a01391d4b87ad45df6cff9af772a2a
aepiercy <39595350+aepiercy@users.noreply.github.com>
2022-10-19 15:26:46 -0700

    Add files via upload

---
+++ UnionFind.java
@@ -0,0 +1,42 @@
+
+/** Disjoint sets of contiguous integers that allows (a) finding whether
+ *  two integers are in the same set and (b) unioning two sets together.  
+ *  At any given time, for a structure partitioning the integers 1 to N, 
+ *  into sets, each set is represented by a unique member of that
+ *  set, called its representative.
+ *  @author Anne Piercy
+ */
+public class UnionFind {
+
+    /** A union-find structure consisting of the sets { 1 }, { 2 }, ... { N }.
+     */
+    public UnionFind(int N) {
+        // FIXME ---
+        parent = new int [N];
+        sizes = new int [N];
+        for(int i = 1; i < N; i++) {
+            parent[i - 1] = i;
+            sizes[i] = -1;
+        }
+    }
+
+    /** Return the representative of the set currently containing V.
+     *  Assumes V is contained in one of the sets.  */
+    public int find(int v) {
+        return 0;  // FIXME
+    }
+
+    /** Return true iff U and V are in the same set. */
+    public boolean samePartition(int u, int v) {
+        return find(u) == find(v);
+    }
+
+    /** Union U and V into a single set, returning its representative. */
+    public int union(int u, int v) {
+        return 0;  // FIXME
+    }
+
+    // FIXME
+    private int[] parent;
+    private int[] sizes;
+}
